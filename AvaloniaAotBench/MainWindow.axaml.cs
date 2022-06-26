060a7ab3fd600ef84f9a5dd6be0969c73f3d5b96
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-27 00:22:54 +0600

    Add controled exit

--- AvaloniaAotBench/MainWindow.axaml.cs
+++ AvaloniaAotBench/MainWindow.axaml.cs
@@ -39,7 +39,11 @@ namespace AvaloniaAotBench
 
             public void Render(IDrawingContextImpl context)
             {
-                Environment.Exit(42);
+                var args = Environment.GetCommandLineArgs();
+                if (args.Length > 1 && args[1] == "crash")
+                {
+                    Environment.Exit(42);
+                }
             }
 
             public Rect Bounds { get; }
