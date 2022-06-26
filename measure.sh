f52168c29eba5f1fe71ee6d72d80fbd4363cbe59
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-27 02:54:44 +0600

    Finish Linux measurements

--- measure.sh
+++ measure.sh
@@ -1,11 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash
 
 function measure
 {
     local SampleName=$1
     local rid=$2
-    local TIME_DATA=$( ( time $SampleName/bin/Release/net6.0/$rid/publish/$SampleName crash 2>&1 | tee tmp.txt 2>&1 >/dev/null ; exit ${PIPESTATUS[0]} ) 2>&1 );
-    if [ $? -ne 42 ]
+    rm tmp.txt -f
+    capture="$(time ($SampleName/bin/Release/net6.0/$rid/publish/$SampleName crash 2>&1) 2>&1)"
+    local EXIT_CODEZ=$?
+    local TIME_DATA="$capture"
+    # echo $EXIT_CODEZ
+    if [ $EXIT_CODEZ -ne 42 ]
     then
         TIME_DATA="-1"
     fi
@@ -13,30 +17,32 @@ function measure
     echo "$TIME_DATA"
 }
 
+echo "Building Regular app"
+rm AvaloniaAotBench/{bin,obj} -rf
+dotnet publish AvaloniaAotBench/AvaloniaAotBench.csproj -c Release -r linux-x64 /p:Mode=None > /dev/null
+echo "Measuring Regular app"
+measure "AvaloniaAotBench" "linux-x64"
+
 echo "Building Trimmed app"
 rm AvaloniaAotBench/{bin,obj} -rf
 dotnet publish AvaloniaAotBench/AvaloniaAotBench.csproj -c Release -r linux-x64 /p:Mode=Trimmed > /dev/null
 echo "Measuring Trimmed app"
 measure "AvaloniaAotBench" "linux-x64"
-echo "Trimmed " $TrimmedTime " ms"
 
 echo "Building R2R app"
 rm AvaloniaAotBench/{bin,obj} -rf
 dotnet publish AvaloniaAotBench/AvaloniaAotBench.csproj -c Release -r linux-x64 /p:Mode=R2R > /dev/null
 echo "Measuring R2R app"
 measure "AvaloniaAotBench" "linux-x64"
-echo "R2R " $R2RTime " ms"
 
 echo "Building R2R Composite app"
 rm AvaloniaAotBench/{bin,obj} -rf
 dotnet publish AvaloniaAotBench/AvaloniaAotBench.csproj -c Release -r linux-x64 /p:Mode=R2R > /dev/null
 echo "Measuring R2R Composite app"
 measure "AvaloniaAotBench" "linux-x64"
-echo "R2R Composite " $R2RCompositeTime " ms"
 
 echo "Building NativeAOT app"
 rm AvaloniaAotBench/{bin,obj} -rf
 dotnet publish AvaloniaAotBench/AvaloniaAotBench.csproj -c Release -r linux-x64 /p:Mode=NativeAOT > /dev/null
 echo "Measuring NativeAOT app"
 measure "AvaloniaAotBench" "linux-x64"
-echo "NativeAOT " $NativeAOTTime " ms"
